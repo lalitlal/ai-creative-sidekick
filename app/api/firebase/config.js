559f814c3173b131b82be6595bd88aa93aef871a
Lalit Lal <lalitlal23@hotmail.com>
2023-08-15 18:32:10 -0400

    trying again with getapps

--- app/api/firebase/config.js
+++ app/api/firebase/config.js
@@ -8,10 +8,8 @@ const decodedBuffer = Buffer.from(
   "base64"
 );
 const decodedJSON = JSON.parse(decodedBuffer.toString());
-export const firebase_app = initializeApp({
-  credential: credential.cert(decodedJSON),
-});
-//   getApps().length === 0
-//     ? initializeApp({ credential: credential.cert(decodedJSON) })
-//     : getApps()[0];
+export const firebase_app =
+  getApps().length === 0
+    ? initializeApp({ credential: credential.cert(decodedJSON) })
+    : getApps()[0];
 export const db = getFirestore(firebase_app);
