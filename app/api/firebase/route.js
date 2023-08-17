aa59de0e62bca8cc3dce8c51e9d2a7e396d56895
Lalit Lal <lalitlal23@hotmail.com>
2023-08-17 09:13:53 -0400

    comment out route

--- app/api/firebase/route.js
+++ app/api/firebase/route.js
@@ -1,34 +1,34 @@
-import { GoogleAuth } from "google-auth-library";
-import { NextResponse } from "next/server";
-import { db } from "./config";
+// import { GoogleAuth } from "google-auth-library";
+// import { NextResponse } from "next/server";
+// import { db } from "./config";
 
-// AUTH :https://stackoverflow.com/questions/74299271/linking-google-credentials-to-vercel
-// https://firebase.google.com/docs/firestore/query-data/get-data#node.js
+// // AUTH :https://stackoverflow.com/questions/74299271/linking-google-credentials-to-vercel
+// // https://firebase.google.com/docs/firestore/query-data/get-data#node.js
 
-export async function GET(request) {
-  // Call the function to make the request
-  const uuid = request.nextUrl.searchParams.get("uuid");
+// export async function GET(request) {
+//   // Call the function to make the request
+//   const uuid = request.nextUrl.searchParams.get("uuid");
 
-  const responseRef = db.collection("responses").doc(uuid);
-  const doc = await responseRef.get();
-  if (!doc.exists) {
-    console.log("No such document!");
-    return NextResponse.json(
-      {
-        message: "No data exists",
-      },
-      {
-        status: 200,
-      }
-    );
-  }
-  const data = doc.data();
-  return NextResponse.json(
-    {
-      message: data.response,
-    },
-    {
-      status: 200,
-    }
-  );
-}
+//   const responseRef = db.collection("responses").doc(uuid);
+//   const doc = await responseRef.get();
+//   if (!doc.exists) {
+//     console.log("No such document!");
+//     return NextResponse.json(
+//       {
+//         message: "No data exists",
+//       },
+//       {
+//         status: 200,
+//       }
+//     );
+//   }
+//   const data = doc.data();
+//   return NextResponse.json(
+//     {
+//       message: data.response,
+//     },
+//     {
+//       status: 200,
+//     }
+//   );
+// }
